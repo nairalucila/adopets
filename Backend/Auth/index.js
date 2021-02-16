16f4b4b43123293c07cb7ed1f6cc955365b8dd4f
naira <nairacoppari@gmail.com>
2021-02-16 17:55:28 -0300

    created two dirs for routes and configuration atauth dir exports routers

--- Backend/Auth/index.js
+++ Backend/Auth/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//router configuration
+app.use(require('./routers/index'));
+
+//midleware
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
