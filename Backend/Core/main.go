8dfb3da97388f83c9091d2912dce8b3fc052e6bb
naira <nairacoppari@gmail.com>
2021-03-09 11:39:23 -0300

    create a web server with two path

--- Backend/Core/main.go
+++ Backend/Core/main.go
@@ -1,23 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	// initiates a gin Engine with the default logger and recovery middleware
-	router := gin.Default()
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
 
-	// sets up a GET API in route /hello that returns the text "World"
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "World",
-		})
-	})
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
 
-	// Run implements a http.ListenAndServe() and takes in an optional Port number
-	// The default port is :8080
-	router.Run()
+	fmt.Fprintf(w, "Hello!")
 }
+
+func inicio(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Inicio de la seccion, dirigete a /hello")
+}
+
+func main() {
+
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/", inicio)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+//https://blog.logrocket.com/creating-a-web-server-with-golang/
