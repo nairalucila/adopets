014d79f6ec86ce647dfac2274f85f0af666b2181
naira <nairacoppari@gmail.com>
2021-04-16 15:56:45 -0300

    It created three forms for login, provisorios and user data

--- Front/adopets/src/app/app.component.ts
+++ Front/adopets/src/app/app.component.ts
@@ -2,23 +2,67 @@ import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   formSingIn = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl("", [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),]),
+    password: new FormControl('', Validators.required,),
   });
 
+
+  formProvisorio = new FormGroup({
+    havePets: new FormControl(''),
+    howPets: new FormControl(''),
+    sizePets: new FormControl(''),
+    agePets: new FormControl(''),
+    genderPets: new FormControl(''),
+
+  });
+
+  formUserData = new FormGroup({
+    nameUser: new FormControl(''),
+    lastnameUser: new FormControl(''),
+    ageUser: new FormControl(''),
+    addresUser: new FormControl(''),
+    neighborhoodUser: new FormControl(''),
+    provinceUser: new FormControl(''),
+    phoneUser: new FormControl(''),
+    secondPhoneUser: new FormControl(''),
+
+  })
+
+  loginSubmitt: boolean;
   title = 'adopets';
+  hide: boolean = true;
 
-  constructor(){ }
+  constructor() {
+    this.loginSubmitt = false;
+  }
 
-  onSubmit(){
-    console.log(this.formSingIn.value)
+  onSubmitLogin() {
+    this.loginSubmitt = true;
+    console.table(this.formSingIn.value.email);
+
+    let emailogin = this.formSingIn.value.email;
+    let passwordlogin = this.formSingIn.value.password;
+
+  }
+
+
+  onSubmitProvi() {
+    console.log(this.formProvisorio.value);
+
+  }
+
+  onSubmitUserData() {
+    console.table(this.formUserData.value);
   }
 
 }
