8002cadf001c775ca0eb6d178e1a1ed0582c5213
naira <nairacoppari@gmail.com>
2021-04-16 18:00:51 -0300

    new staffs, forms and endpoints

--- Backend/Auth/controllers/index.controller.js
+++ Backend/Auth/controllers/index.controller.js
@@ -1,8 +1,23 @@
-const dashboard = (req, res)=>{
-    res.send('Hi');
+//conexion con postgres
+
+const {Pool} = require("pg");
+new Pool({
+    
+})
+
+
+const login = (req, res)=>{
+    res.send('Sing in');
+};
+
+const register = (req, res)=>{
+    res.send('Sing up');
 };
 
 
+
+
 module.exports = {
-    dashboard
+    login,
+    register
 }
